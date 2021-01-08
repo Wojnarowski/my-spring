3e06ed43e81f47ff12cc02e998d9e5222cc2ff10
guozhizhong <guo_zhizhong@sina.com>
2021-01-08 10:52:42 +0800

    提交代码

--- my-spring/src/main/java/TestApplication.java
+++ my-spring/src/main/java/TestApplication.java
@@ -7,8 +7,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestApplication {
 	public static void main(String[] args) {
 		 //进入 ClassPathXmlApplicationContext
-		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("XXX.xml");
-		Object object = applicationContext.getBean("a");
-		System.out.println("name = " + object);
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+		Person person = (Person) applicationContext.getBean("person");
+		System.out.println(person.toString());
 	}
 }
