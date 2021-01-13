df7b7a1718ad22aaa8a69d9317977924e524aa36
guozhizhong <guo_zhizhong@sina.com>
2021-01-14 00:50:54 +0800

    自定义标签

--- my-spring/src/main/java/TestApplication.java
+++ my-spring/src/main/java/TestApplication.java
@@ -1,5 +1,6 @@
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import selftag.User;
 
 /**
  * 源码阅读测试类
@@ -7,9 +8,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestApplication {
 	public static void main(String[] args) {
 		 //进入 ClassPathXmlApplicationContext
-		//ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
-		MyClasspathXmlApplicationContext applicationContext = new MyClasspathXmlApplicationContext("bean.xml");
-		Person person = (Person) applicationContext.getBean("person");
-		System.out.println(person.toString());
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("bean.xml");
+		//MyClasspathXmlApplicationContext applicationContext = new MyClasspathXmlApplicationContext("bean.xml");
+//		Person person = (Person) applicationContext.getBean("person");
+//		System.out.println(person.toString());
+		User user = (User)applicationContext.getBean("guozhizhong");
+		System.out.println(user.toString());
 	}
 }
---
+++ my-spring/src/main/java/selftag/UserNamespaceHandler.java
@@ -0,0 +1,10 @@
+package selftag;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class UserNamespaceHandler extends NamespaceHandlerSupport {
+	@Override
+	public void init() {
+		registerBeanDefinitionParser("user",new UserBeanDefinitionBeanParse());
+	}
+}
