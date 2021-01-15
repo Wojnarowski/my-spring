6933e5cca8dc430df50dd0f86609de4b0cd2333a
guozhizhong <guo_zhizhong@sina.com>
2021-01-15 14:20:01 +0800

    提交代码

---
+++ my-spring/src/main/java/selfEditor/AddressPropertyEditor.java
@@ -0,0 +1,12 @@
+package selfEditor;
+
+import java.beans.PropertyEditorSupport;
+
+public class AddressPropertyEditor extends PropertyEditorSupport {
+
+	@Override
+	public void setAsText(String s) throws IllegalArgumentException {
+
+
+	}
+}
